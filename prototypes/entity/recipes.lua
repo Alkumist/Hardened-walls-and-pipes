facdf5a90e94e37ee76bd3427df005ed058049a1
Alkumist <ponyyouman@gmail.com>
2024-10-11 00:29:43 -0600

    fixed spacing

--- prototypes/entity/recipes.lua
+++ prototypes/entity/recipes.lua
@@ -1,46 +1,49 @@
-	if mods["aai-industry"] then
-		data:extend({{
-	    			-- data.raw["wall"]["concrete-wall"].next_upgrade = "hardened-wall"
-				type = "recipe",
-				name = "hardened-wall",
-				enabled = false,
-				energy_required = 0.5,
-				ingredients = {
-					{type = "item", name = "iron-stick", amount = 2},
-					{type = "item", name = "concrete-wall", amount = 1},
-				},
-				result = "hardened-wall",
-			}
-		})
-	elseif mods["ConcreteWall"] then
-		data:extend({{
-	    			-- data.raw["wall"]["stone-wall-2"].next_upgrade = "hardened-wall"
-				type = "recipe",
-				name = "hardened-wall",
-				enabled = false,
-				energy_required = 0.5,
-				ingredients = {
-					{type = "item", name = "iron-stick", amount = 2},
-					{type = "item", name = "stone-wall-2", amount = 1},
-				},
-				result = "hardened-wall",
-			}
-		})
-	else
-		data:extend({{
-	    			type = "recipe",
-				name = "hardened-wall",
-				enabled = false,
-				energy_required = 0.5,
-				ingredients = {
-			    		{type = "item", name = "concrete", amount = 5},
-					{type = "item", name = "iron-stick", amount = 2},
-					{type = "item", name = "stone-wall", amount = 2},
-				},
-				result = "hardened-wall",
-			}
-		})
-	end
+if mods["aai-industry"] then
+	data:extend({
+		{
+			-- data.raw["wall"]["concrete-wall"].next_upgrade = "hardened-wall"
+			type = "recipe",
+			name = "hardened-wall",
+			enabled = false,
+			energy_required = 0.5,
+			ingredients = {
+				{type = "item", name = "iron-stick", amount = 2},
+				{type = "item", name = "concrete-wall", amount = 1},
+			},
+			result = "hardened-wall",
+		}
+	})
+elseif mods["ConcreteWall"] then
+	data:extend({
+		{
+	    	-- data.raw["wall"]["stone-wall-2"].next_upgrade = "hardened-wall"
+			type = "recipe",
+			name = "hardened-wall",
+			enabled = false,
+			energy_required = 0.5,
+			ingredients = {
+				{type = "item", name = "iron-stick", amount = 2},
+				{type = "item", name = "stone-wall-2", amount = 1},
+			},
+			result = "hardened-wall",
+		}
+	})
+else
+	data:extend({
+		{
+	    	type = "recipe",
+			name = "hardened-wall",
+			enabled = false,
+			energy_required = 0.5,
+			ingredients = {
+				{type = "item", name = "concrete", amount = 5},
+				{type = "item", name = "iron-stick", amount = 2},
+				{type = "item", name = "stone-wall", amount = 2},
+			},
+			result = "hardened-wall",
+		}
+	})
+end
 data:extend({
 	{
 		type = "recipe",
